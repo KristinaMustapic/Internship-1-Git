2b48cc7b8e66bcd52c975667f4c3207ec6052db4
KristinaMustapic <kristina.mustapic10@gmail.com>
2020-11-21 12:16:10 +0100

    Added GitHomework

---
+++ GitHomeworkApp/GitHomeworkApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHomeworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
